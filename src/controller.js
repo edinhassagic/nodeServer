1ef825f0381f11ab9e783dc049b6961f52273d66
edin hasagic <edinhassagic@gmail.com>
2021-04-21 16:13:04 +0200

    node project

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,6 @@
 const root = (req, res) => {
 
-  res.end("Hello Node Server!");
+  res.end("Hello Node Server, how are you!");
 };
 
 const text = (req, res) => {
@@ -14,4 +14,4 @@ const html = (req, res) => {
   const param = req.params.param || 'Html';
   res.end(`<h1>Hello ${param} Server!</h1>`);
 };
-module.exports = { root, text, html };
+export  { root, text, html }
